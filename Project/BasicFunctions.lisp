95fb3b3eb4a50d6776c40e4159db04ef84ad1ffd
Uros <uros.aleksandrovic@outlook.com>
2019-12-05 19:18:54 +0100

    Fixing major bugs in BasicFunctions and writing test cases for them

--- Project/BasicFunctions.lisp
+++ Project/BasicFunctions.lisp
@@ -1,28 +1,22 @@
-
-;;This funcion returnes list that represents the printing stack from one tile in our board
-(defun getTilePrintList (tileSize tileList) 
-    (cond ((or (equalp tileSize 0) (null tileList)) '())
-        ((> (- tileSize (length tileList)) 0) (cons '- (getTilePrintList (- tileSize 1) tileList)) )
-        (t(cons (car tileList) (getTilePrintList (- tileSize 1) (cdr tileList))))))
-
-
 ;;This function returns sub list of a list
-;;Arguments are list, first and last element of sublist
-(defun getSubList (list firstElement lastElement) 
-    (cond ((or (< firstElement 0) (> lastElement (length list)))
+;;Arguments are list, first element of sublist and length of sublist
+(defun getSubList (list firstElement lengthOfSublist) 
+    (cond ((or (< firstElement 0) (> (+ lengthOfSublist firstElement) (length list)))
             (format t "First and last elements got to be in the range of given list"))
         ((null list) '())
-        ((> firstElement 0) (getSubList (cdr list) (- firstElement 1) lastElement))
-        ((> lastElement 0) (cons (car list) (getSubList (cdr list) firstElement (- lastElement 1))))))
+        ((> firstElement 0) (getSubList (cdr list) (- firstElement 1) lengthOfSublist))
+        ((> lengthOfSublist 0) (cons (car list) (getSubList (cdr list) firstElement (- lengthOfSublist 1))))))
 
 
 ;;Reads character from standard intput and puts in the list. Additionaly it shows message before reading.
-(defun readChar (Message) (progn (format t  Message) (list(read))))
+(defun readChar (Message) (progn (format t  "~A " Message) (list (read))))
 
 ;;Returnes the index of element in list
 (defun indexOf (element list)
-    (cond ((equalp element (car list)) 0)
-        (t(+ 1 (indexOf element (cdr list))))))
+    (cond ((null list) '())
+        ((equalp element (car list)) 0)
+        (t(let ((index (indexOf element (cdr list))))
+               (if (null index) '() (+ 1 index))))))
 
 ;;Returnes true if index of element is even
 (defun evenIndexp (element list) (evenp (indexOf element list)))
@@ -45,10 +39,10 @@
 ;;Counts number of given element in the list
 (defun countEl (element list) 
     (cond ((null list) 0)
-          ((equal element (car list)) (+ 1 (count element (cdr list)))) 
-          (t(count element (cdr list)))))
+          ((equal element (car list)) (+ 1 (countEl element (cdr list)))) 
+          (t(countEl element (cdr list)))))
 
-;;Inserts element in the list at givent index and pushing out previous element from that place
+;;Inserts element in the list at givent index and deleting previous element from that place
 (defun insertElement (element list index)
     (cond ((null list) '())
           ((equal index 0) (cons element (cdr list)))
@@ -61,6 +55,6 @@
 
 ;;Returnes last N elements of passed list
 (defun getLastN (numOfElements list)
-(cond ((null list) '())
-      ((> numOfElements 0) (getLastN (- numOfElements 1) (cdr list)))
-      (t (list EMPTY))))
+(cond ((null list) (list EMPTY))
+      ((> numOfElements 1) (getLastN (- numOfElements 1) (cdr list)))
+      (t list)))
--- Project/Testers.lisp
+++ Project/Testers.lisp
@@ -1,8 +1,24 @@
 (load "Byte.lisp")
 
+;;TESTING BASIC FUNCTIONS-------------------------------
+
+;;(print (getSubList '(0 1 2 3 4 5 6 7 8 9) 3 4))
+;;(print (readChar "Unesi jedan karakter"))
+;;(print (indexOf '2 '(1 2 3)))
+;;(print (evenIndexp '1 '(1 2 3)))
+;;(print (div 13 2))
+;;(print (memberList '(1 2) '(3 5 6 (1 2))))
+;;(print (join '(1 2 (4 5)) '(3 2 (4 5))))
+;;(print (countEl '(1 2) '(2 3 2 5 (1 2))))
+;;(print (insertElement '(10) '(0 1 2 3 4 5 6) '1))
+;;(print (getFirstN 3 '(0 1 2 3 4)))
+(print (getLastN '7 '(1 2 3 4 5)))
 
 
-;;Sto sam ovo stavio bese? za positionsOfSym
-;;(+ (* (indexOf (caar stateToCheck) PossibleRows) dimension)
-;;                                (mod (indexOf (caar stateToCheck) PossibleRows) 2))
+;(setq myState (create-initial-state 8))
+;(setq myMove (create-move '(B 2) '(C 3) 0))
+;(print myMove)
+;(print (playMove myMove myState))
 
+;(setq newState (copyState myState))
+;(print newState)
