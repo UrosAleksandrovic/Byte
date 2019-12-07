0f866e78e67756fb070b00160f725926f0f627e0
Uros <uros.aleksandrovic@outlook.com>
2019-12-08 00:17:07 +0100

    Fixing major bugs in Byte file. Complete testers wrote in Testers.

--- Project/Byte.lisp
+++ Project/Byte.lisp
@@ -33,15 +33,16 @@
     (I ((O) (O) (O) (O) (O))) 
     (J ((-) (-) (-) (-) (-)))))
 
+;;Saving list of possible rows in variable. List is used as helper list for faster search
 (setf PossibleRows '(A B C D E F G H I J))
 
-;;Storing stacks that are size of 8 and not on board anymore
+;;Storing stacks that are size of 8 and not on board anymore.
 (setf FinalStacks '())
 
 ;;Checking if game is over and returning winner if it is, null if it's not
 (defun checkFinalState ()
     (cond ((not (equal (length FinalStacks) 3)) '())
-          (t(if (>(countEl BLACK FinalStacks) 1) BLACK WHITE))))
+          (t(if (> (countEl BLACK FinalStacks) 1) BLACK WHITE))))
 
 ;;-------------------------------------------
 ;;Move structure
@@ -93,49 +94,52 @@
                  (validTilesForMove startingTile (cdr nearestTiles) dimension)))))
 
 ;;Check altitude rule for two tiles
-(defun altitudeCheck (startingTile targetedTile startingAltitude stateOfGame) 
-    (let ((heightOfStartingStack (length (valueOfTile startingTile stateOfGame)))
-          (heightOfTargetetStack (length (valueOfTile targetedTile stateOfGame))))
+(defun altitudeCheck (startingTile targetedTile startingAltitude currentGameBoard) 
+    (let ((heightOfStartingStack (length (valueOfTile startingTile currentGameBoard)))
+          (heightOfTargetetStack (length (valueOfTile targetedTile currentGameBoard))))
           (cond ((> (+ heightOfStartingStack heightOfTargetetStack) 8) '())
                 ((> startingAltitude heightOfTargetetStack) '())
                 (t t))))
 
 ;;Returnes the resault targeted stack after move
-(defun calculateResaultStack (startingTile targetedTile startingAltitude stateOfGame)
-    (let ((resaultStack (append (getFirstN (- (length (formatEmptyStack (valueOfTile startingTile stateOfGame))) startingAltitude)
-                                           (valueOfTile startingTile stateOfGame))
-                                (formatEmptyStack (valueOfTile targetedTile stateOfGame)))))
+(defun calculateResaultStack (startingTile targetedTile startingAltitude currentGameBoard)
+    (let ((resaultStack (append (getFirstN (- (length (formatEmptyStack (valueOfTile startingTile currentGameBoard))) startingAltitude)
+                                           (valueOfTile startingTile currentGameBoard))
+                                (formatEmptyStack (valueOfTile targetedTile currentGameBoard)))))
          (if (equal (length resaultStack) 8) 
              (progn (setf FinalStacks (cons (car resaultStack) FinalStacks)) '())
              resaultStack)))
 
 ;;Returnes the resault of starting tile after the move
-(defun calculateNewStartingTileStack (startingTile startingAltitude stateOfGame)
-    (getLastN startingAltitude (formatEmptyStack (valueOfTile startingTile stateOfGame))))
+(defun calculateNewStartingTileStack (startingTile startingAltitude currentGameBoard)
+    (let ((startingTileStack (valueOfTile startingTile currentGameBoard)))
+         (cond ((>= startingAltitude (length startingTileStack)) startingTileStack)
+               (t(getLastN startingAltitude startingTileStack)))))
 
-;;Alters one Row of the state after the move
-(defun insertNewTileStack (newTileStack tileToAlter stateOfGame)
-    (cond ((null stateOfGame) '())
-          ((equal (car tileToAlter) (caar stateOfGame))
-            (cons (list (car tileToAlter) (insertElement newTileStack (cadar stateOfGame) (cadr tileToAlter)))
-                  (cdr stateOfGame)))
-          (t(cons (car stateOfGame) (insertNewTileStack newTileStack tileToAlter (cdr stateOfGame))))))
+;;Alters one Row of the game board after the move
+(defun insertNewTileStack (newTileStack tileToAlter currentGameBoard)
+    (cond ((null currentGameBoard) '())
+          ((equal (car tileToAlter) (caar currentGameBoard))
+            (cons (list (car tileToAlter) (insertElement newTileStack (cadar currentGameBoard) (div (cadr tileToAlter) 2)))
+                  (cdr currentGameBoard)))
+          (t(cons (car currentGameBoard) (insertNewTileStack newTileStack tileToAlter (cdr currentGameBoard))))))
 
-;;Returnes new state when move is performed
-(defun alterState (moveToPerforme stateToAlter)
+;;Returnes new game board when move is performed
+(defun alterState (moveToPerforme gameBoardToAlter)
     (insertNewTileStack (calculateNewStartingTileStack (move-previousTile moveToPerforme)
                                                        (move-height moveToPerforme)
-                                                       stateToAlter)
+                                                       gameBoardToAlter)
                         (move-previousTile moveToPerforme)
                         (insertNewTileStack (calculateResaultStack (move-previousTile moveToPerforme)
                                                                            (move-nextTile moveToPerforme)
                                                                            (move-height moveToPerforme)
-                                                                           stateToAlter)
-                                                  (move-nextTile moveToPerforme) stateToAlter)))
+                                                                           gameBoardToAlter)
+                                                  (move-nextTile moveToPerforme) gameBoardToAlter)))
 
 ;;Checks if move exists
 (defun moveExistsp (moveToCheck stateOfGame)
-    (let* ((nextTileStack (valueOfTile (move-nextTile moveToCheck) (state-boardValues stateOfGame))))
+    (let* ((nextTileStack (valueOfTile (move-nextTile moveToCheck) (state-boardValues stateOfGame)))
+           (previousTileStack (valueOfTile (move-previousTile moveToCheck) (state-boardValues stateOfGame))))
     (and (moveTilesCheckp moveToCheck (state-dimension stateOfGame))
         (tileOutOfBoundsp (move-previousTile moveToCheck) (state-dimension stateOfGame))
         (tileOutOfBoundsp (move-nextTile moveToCheck) (state-dimension stateOfGame))
@@ -143,11 +147,25 @@
         (memberList (move-nextTile moveToCheck) (validTilesForMove
                                                  (move-previousTile moveToCheck)
                                                  (nearestTile (move-previousTile moveToCheck)
-                                                              (positionsOfSym (state-boardValues stateOfGame)
-                                                                              (car nextTileStack)
-                                                                              (state-dimension stateOfGame)
-                                                                              'A 0)) (state-dimension stateOfGame))))))
+                                                              (tilesOfValidStacks (state-boardValues stateOfGame)
+                                                                              previousTileStack
+                                                                              '())) (state-dimension stateOfGame))))))
 
+
+;;
+(defun playMove (moveToPlay stateOfGame) 
+    (alterState moveToPlay (copyState stateOfGame)))
+
+;;
+(defun getAllPossibleStates (currentState possibleMoves)
+    (cond ((null possibleMoves) '())
+          (t(cons (list (car possibleMoves) (alterState (car possibleMoves) currentState)) 
+                  (getAllPossibleStates currentState (cdr possibleMoves))))))
+
+;;Get all possible moves to performe starting from the given tile
+(defun allposibleMovesFromGivenTile (curentTile validTilesForMove)
+    (cond ((null validTilesForMove) '())
+          (t(cons (create)))))
 ;;-------------------------------------------
 
 ;;State structure. Represents the state of game that player can find himself in.
@@ -159,6 +177,9 @@
     dimension
     boardValues)
 
+;;Copies our state and returnes new
+(defun copyState (stateToCopy) 
+(create-state (state-dimension stateToCopy) (state-boardValues stateToCopy)))
 
 ;;Option function for printing State on standard outstream
 (defun print-board (stateToPrint outputStream)
@@ -175,17 +196,17 @@
 (defun drawFirstRow () 
     (format t "     ~{~a~^    ~}" '(1 2 3 4 5 6 7 8)))
 
-;;Helper function for printing one State row
-(defun drawRow (stateRow parity) 
+;;Helper function for printing one Board row
+(defun drawRow (boardRow parity) 
     (if (equalp parity t) 
         (prog1 
-            (prog1 (format t "~%   ") (drawSingleRow (getSingleRowList (cadr stateRow) 1)))
-            (prog1 (format t "~%~a  " (car stateRow)) (drawSingleRow (getSingleRowList (cadr stateRow) 2)))
-            (prog1 (format t "~%   ") (drawSingleRow (getSingleRowList (cadr stateRow) 3))))
+            (prog1 (format t "~%   ") (drawSingleRow (getSingleRowList (cadr boardRow) 1)))
+            (prog1 (format t "~%~a  " (car boardRow)) (drawSingleRow (getSingleRowList (cadr boardRow) 2)))
+            (prog1 (format t "~%   ") (drawSingleRow (getSingleRowList (cadr boardRow) 3))))
         (prog1 
-            (prog1 (format t "~%        ") (drawSingleRow (getSingleRowList (cadr stateRow) 1)))
-            (prog1 (format t "~%~a       " (car stateRow)) (drawSingleRow (getSingleRowList (cadr stateRow) 2)))
-            (prog1 (format t "~%        ") (drawSingleRow (getSingleRowList (cadr stateRow) 3))))
+            (prog1 (format t "~%        ") (drawSingleRow (getSingleRowList (cadr boardRow) 1)))
+            (prog1 (format t "~%~a       " (car boardRow)) (drawSingleRow (getSingleRowList (cadr boardRow) 2)))
+            (prog1 (format t "~%        ") (drawSingleRow (getSingleRowList (cadr boardRow) 3))))
         ))
 
 ;;Helper function that makes list for printing out of given values of a row
@@ -198,6 +219,13 @@
 (defun drawSingleRow (singleRowList)
     (cond ((null singleRowList) (format t ""))
         (t(prog1 (format t "~{~a~^ ~}     " (car singleRowList)) (drawSingleRow (cdr singleRowList))))))
+
+
+;;This funcion returnes list that represents the printing stack from one tile in our board
+(defun getTilePrintList (tileSize tileList) 
+    (cond ((or (equalp tileSize 0) (null tileList)) '())
+        ((> (- tileSize (length tileList)) 0) (cons '- (getTilePrintList (- tileSize 1) tileList)) )
+        (t(cons (car tileList) (getTilePrintList (- tileSize 1) (cdr tileList))))))
 
 ;;Helper function to create initial state of given dimensions
 (defun create-initial-state (dimension) 
@@ -225,13 +253,13 @@
 
 ;;Predicate for determinating whether the first tile and second tile are on the same diagonal
 (defun sameMainDiagonal (firstTile secondTile dimension) 
-(if (= (mod (tileIndex (car firstTile) (cadr firstTile) PossibleRows dimension) (- dimension 1)) 
-        (mod (tileIndex (car secondTile) (cadr secondTile) PossibleRows dimension) (- dimension 1))) t '()))
+(if (= (mod (tileIndex (car firstTile) (cadr firstTile) PossibleRows dimension) (+ dimension INCREMENT)) 
+        (mod (tileIndex (car secondTile) (cadr secondTile) PossibleRows dimension) (+ dimension INCREMENT))) t '()))
 
 ;;Predicate for determinating whether the first tile and second tile are on the same side diagonal
 (defun sameSideDiagonalp (firstTile secondTile dimension)
-(if (= (mod (tileIndex (car firstTile) (cadr firstTile) PossibleRows dimension) (+ dimension 1)) 
-        (mod (tileIndex (car secondTile) (cadr secondTile) PossibleRows dimension) (+ dimension 1))) t '()))
+(if (= (mod (tileIndex (car firstTile) (cadr firstTile) PossibleRows dimension) (+ dimension DECREMENT)) 
+        (mod (tileIndex (car secondTile) (cadr secondTile) PossibleRows dimension) (+ dimension DECREMENT))) t '()))
 ;;Checks if tile is valid for move. Argument must be index of tile that we get using tileIndex function.
 ;;Only Black tiles on the board are valid.
 (defun checkTilep (tileIndex dimension) 
@@ -245,23 +273,40 @@
             (> (cadr tileToCheck) dimension)
             (< (cadr tileToCheck) 0)) '() t))
 ;;Returnes the stack on the given tile. Tile is represented as '(row collum)
-(defun valueOfTile (tile state) 
-    (cond ((null state) (print "Not valid tile for this state"))
-        ((equalp (car tile) (caar state)) (nth  (div (cadr tile) 2) (car (cdar state))))
-        (t (valueOfTile tile (cdr state)))))
+(defun valueOfTile (tile currentGameBoard) 
+    (cond ((null currentGameBoard) (print "Not valid tile for this state"))
+        ((equalp (car tile) (caar currentGameBoard)) (nth  (div (cadr tile) 2) (cadar currentGameBoard)))
+        (t (valueOfTile tile (cdr currentGameBoard)))))
 
 ;;Returnes the diagonal tile with given directions
 (defun diagonalTile (startingTile rowDirection columnDirection) 
     (list (nth (+ (indexOf (car startingTile) PossibleRows) rowDirection) PossibleRows) (+ (cadr startingTile) columnDirection)))
-;Returnes all positions that have given simbol on the top
-(defun positionsOfSym (stateToCheck symbol dimension startingRow startingColumn) 
-    (cond ((null stateToCheck) '())
-        ((member (caar stateToCheck) PossibleRows)
-            (append (positionsOfSym (cadar stateToCheck) symbol dimension (caar stateToCheck) (mod (indexOf (caar stateToCheck) PossibleRows) 2))
-                    (positionsOfSym (cdr stateToCheck) symbol dimension '() '0)))
-        (t (if (equalp (caar stateToCheck) symbol) 
-               (cons (list startingRow startingColumn) (positionsOfSym (cdr stateToCheck) symbol dimension startingRow (+ 2 startingColumn)))
-               (positionsOfSym (cdr stateToCheck) symbol dimension startingRow (+ 2 startingColumn))))))
+
+;Returnes all tiles that have given simbol on the top
+(defun tilesOfValidStacks (currentGameBoard startingStackValue currentTile) 
+    (cond ((null currentGameBoard) '())
+          ((member (caar currentGameBoard) PossibleRows)
+                (append (tilesOfValidStacks (cadar currentGameBoard)
+                                            startingStackValue 
+                                            (list (caar currentGameBoard) (mod (indexOf (caar currentGameBoard) PossibleRows) 2)))
+                        (tilesOfValidStacks (cdr currentGameBoard) startingStackValue '())))
+          (t(if (checkMergingPosibility startingStackValue (car currentGameBoard))
+                (cons currentTile (tilesOfValidStacks (cdr currentGameBoard)
+                                                      startingStackValue
+                                                      (list (car currentTile) (+ 2 (cadr currentTile)))))
+                (tilesOfValidStacks (cdr currentGameBoard)
+                                                      startingStackValue
+                                                      (list (car currentTile) (+ 2 (cadr currentTile))))))))
+
+;;Returnes t if mearging of two stacks is possible
+(defun checkMergingPosibility (startingStack targetedStack)
+    (let* ((startingHeight (length startingStack))
+           (targetedHeight (length targetedStack))
+           (symbolIndexes (findIndexOf (car (last startingStack)) startingStack 0))
+           (validAltitudeIndexes (returnIfGreaterOrEqual (- startingHeight targetedHeight) symbolIndexes)))
+           (cond ((and (< (- startingHeight targetedHeight) 0) (<= (+ startingHeight targetedHeight) 8)) t)
+                 ((and (not (null validAltitudeIndexes)) (<= (+ targetedHeight (+ INCREMENT (car validAltitudeIndexes))) 8)) t)
+                 (t '()))))
 
 ;Returnes number of moves needed to get from starting tile to targeted tile
 (defun numOfMoves (startingTile targetedTile)
@@ -276,29 +321,30 @@
           (t(let* ((bestTiles (nearestTile startingTile (cdr listOfTiles)))
                   (currentBest (numOfMoves startingTile (car bestTiles)))
                   (possibleBest (numOfMoves startingTile (car listOfTiles))))
-                (cond ((= currentBest possibleBest) (cons (car listOfTiles) bestTiles))
+                (cond ((= possibleBest 0) bestTiles)
+                      ((= currentBest possibleBest) (cons (car listOfTiles) bestTiles))
                       ((> currentBest possibleBest) (list(car listOfTiles)))
                       ((< currentBest possibleBest) bestTiles))))))
 
 ;;Returnes tiles that are possible to play with given starting tile and one nearest tile
 (defun availableTileForGivenTile (startingTile singleNearestTile dimension)
     (let ((quadrantOfTarget (tileQuadrant startingTile singleNearestTile)))
-         (cond ((= quadrantOfTarget 1) (if (sameMainDiagonal startingTile singleNearestTile dimension)
+         (cond ((= quadrantOfTarget 1) (if (sameSideDiagonalp startingTile singleNearestTile dimension)
                                            (diagonalTile startingTile DECREMENT INCREMENT)
                                             (list 
                                                 (diagonalTile startingTile DECREMENT INCREMENT)
                                                 (diagonalTile startingTile INCREMENT INCREMENT))))
-               ((= quadrantOfTarget 2) (if (sameSideDiagonalp startingTile singleNearestTile dimension)
+               ((= quadrantOfTarget 2) (if (sameMainDiagonal startingTile singleNearestTile dimension)
                                             (diagonalTile startingTile DECREMENT DECREMENT)
                                             (list 
                                                 (diagonalTile startingTile DECREMENT DECREMENT)
                                                 (diagonalTile startingTile DECREMENT INCREMENT))))
-               ((= quadrantOfTarget 3) (if (sameMainDiagonal startingTile singleNearestTile dimension)
+               ((= quadrantOfTarget 3) (if (sameSideDiagonalp startingTile singleNearestTile dimension)
                                             (diagonalTile startingTile INCREMENT DECREMENT)
                                             (list 
                                                 (diagonalTile startingTile INCREMENT DECREMENT)
                                                 (diagonalTile startingTile DECREMENT DECREMENT))))
-               ((= quadrantOfTarget 4) (if (sameSideDiagonalp startingTile singleNearestTile dimension)
+               ((= quadrantOfTarget 4) (if (sameMainDiagonal startingTile singleNearestTile dimension)
                                             (diagonalTile startingTile INCREMENT INCREMENT)
                                             (list 
                                                 (diagonalTile startingTile INCREMENT INCREMENT)
--- Project/Testers.lisp
+++ Project/Testers.lisp
@@ -12,13 +12,62 @@
 ;;(print (countEl '(1 2) '(2 3 2 5 (1 2))))
 ;;(print (insertElement '(10) '(0 1 2 3 4 5 6) '1))
 ;;(print (getFirstN 3 '(0 1 2 3 4)))
-(print (getLastN '7 '(1 2 3 4 5)))
+;;(print (getLastN '7 '(1 2 3 4 5)))
+;;(print (findIndexOf '2 '(0 1 2 3 2 4 5 6 2) '0))
+;;(print (returnIfGreaterOrEqual '3 '(0 1 2 3 4 5 6 7)))
+;;-----------------------------------------------------
 
+;;TESTING BYTE GAME FUNCTIONS--------------------------
 
-;(setq myState (create-initial-state 8))
-;(setq myMove (create-move '(B 2) '(C 3) 0))
-;(print myMove)
-;(print (playMove myMove myState))
+;;;;Moves
 
-;(setq newState (copyState myState))
-;(print newState)
+;;(setq FinalStacks (list BLACK WHITE BLACK))
+;;(print (checkFinalState))
+
+;;(setq myMove (create-move '(B 2) '(C 3) 0))
+;;(setq myMove (create-move '() '() 0))
+;;(readMove myMove)
+;;(print myMove)
+
+(setq myMove (create-move '(B 1) '(C 2) 0))
+;;(print (moveTilesCheckp myMove 8))
+
+;;(print (validTilesForMove '(B 1) (nearestTile '(B 1) (tilesOfValidStacks InitialState8x8 '(X) '())) 8))
+
+;;(print (altitudeCheck '(B 1) '(C 5) '0 InitialState8x8))
+;;(print (calculateResaultStack '(B 2) '(C 5) '0 InitialState8x8))
+;;(print (calculateNewStartingTileStack '(B 2) '0 InitialState8x8))
+;;(print (insertNewTileStack (calculateResaultStack '(B 2) '(C 5) '0 InitialState8x8) '(C 5) InitialState8x8))
+;;(print (alterState myMove InitialState8x8))
+
+;;States
+
+(setq myState (create-state 8 InitialState8x8))
+;(print myState)
+;;(setq copyState (copyState myState))
+;;(print copyState)
+
+;;(print (formatEmptyStack '(-)))
+;;(drawFirstRow)
+;;(drawRow (cadr InitialState8x8) t)
+;;(drawRow (cadr InitialState8x8) nill)
+;;(print (getSingleRowList (cadar InitialState8x8) 1))
+;;(drawSingleRow (getSingleRowList (cadar InitialState8x8) 1))
+;;(print (getTilePrintList 9 (caadar InitialState8x8)))
+;;(setq myState (create-initial-state 8))
+
+;;(print (tileIndex 'D '0 PossibleRows '8))
+;;(print (tileQuadrant '(B 4) '(D 4)))
+;;(print (sameMainDiagonal '(B 4) '(C 5) 8))
+;;(print (sameSideDiagonalp '(B 4) '(C 5) 8))
+;;(print (checkTilep (tileIndex 'D '1 PossibleRows '8) 8))
+;;(print (tileOutOfBoundsp '(B 8) 8))
+
+;;(print (valueOfTile '(F 2) InitialState8x8))
+;;(print (checkMergingPosibility '(O O O X X X) '(O O O O X)))
+;;(print (tilesOfValidStacks InitialState8x8 '(X) '()))
+;;(print (numOfMoves '(A 0) '(D 5)))
+;;(print (nearestTile '(B 1) (tilesOfValidStacks InitialState8x8 '(X) '())))
+;;(print (availableTileForGivenTile '(B 1) '(A 0) 8))
+
+;;(print (moveExistsp myMove myState))
